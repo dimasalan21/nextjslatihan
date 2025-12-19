f63d9bf24779cccb5ca900d9542415477614ddd6
dimasalan21 <dimasalan540@gmail.com>
2025-12-20 01:10:22 +0700

    Fix login route final

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+import { handlers } from "@/auth"; // Mengambil pengaturan dari file auth.ts kamu
+export const { GET, POST } = handlers;
---
+++ auth.ts
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { z } from "zod";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`
+      SELECT * FROM users WHERE email=${email}
+    `;
+    return user[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    return undefined;
+  }
+}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        const parsed = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (!parsed.success) return null;
+
+        const { email, password } = parsed.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.password
+        );
+
+        if (!passwordsMatch) return null;
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
